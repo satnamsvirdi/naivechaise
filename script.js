2468d3da1cb5440d1324c99d26901d01589f4754
Glitch (hello-webpage) <none>
2020-12-17 12:59:26 +0000

    👍💴 Checkpoint ./script.js:21521835/113 ./custom-styles.css:21521835/254 ./index.html:21521835/62

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const bagBtn = document.querySelector('#bag');
+const body = document.querySelector('body');
 const closeBag = document.querySelector('#close-bag');
 const aside = document.querySelector('aside');
 
 const toggleBag = (e) => {
   e.preventDefault()
   aside.classList.toggle("hidden")
+  body.classList.toggle("hide")
 }
 
 bagBtn.addEventListener('click', toggleBag)
