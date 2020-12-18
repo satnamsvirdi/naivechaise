7835b84809890ba7ee6bdd6bdd4de0d87f2f2210
Glitch (hello-webpage) <none>
2020-12-18 13:32:20 +0530

    Opimized for Mobile breakpoint

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const closeBag = document.querySelector('#close-bag');
 const aside = document.querySelector('aside');
 const personDropdown = document.querySelector('#person-dropdown');
 const personDropdownBtn = document.querySelector('#person-dropdown-btn');
+const mobileMenuBtn = document.querySelector('#mobile-menu-btn');
+const mobileMenuDropdown = document.querySelector('#mobile-menu-dropdown');
 
 const toggleBag = (e) => {
   e.preventDefault()
@@ -18,5 +20,11 @@ const toggleDropdown = (e) => {
   e.preventDefault();
   personDropdown.classList.toggle("hidden")
 }
-
 personDropdownBtn.addEventListener('click', toggleDropdown)
+
+const toggleMobileDropdown = (e) => {
+  e.preventDefault();
+  mobileMenuDropdown.classList.toggle("hidden")
+}
+
+mobileMenuBtn.addEventListener('click', toggleMobileDropdown)
