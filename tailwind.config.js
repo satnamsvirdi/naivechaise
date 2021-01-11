bdf1e9f7dc546b0bd3e3647392560ff9d1aacb2c
Glitch (hello-webpage) <none>
2021-01-11 19:57:32 +0530

    Add 2xl viewport

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    }
   },
   variants: {
     extend: {},
